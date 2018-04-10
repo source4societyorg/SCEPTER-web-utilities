d7e83f461755641ab7c64667a184ffdff4827344
Daniel Fredriksen <dfredriksen@cyint.technology>
2018-04-10 09:03:31 -0700

    Fixed issues with media query and styles tests

--- tests/mediaQuery.test.js
+++ tests/mediaQuery.test.js
@@ -1,7 +1,7 @@
 import mediaQuery from '../src/mediaQuery';
 
 test('mediaQuery will select the choice depending on the window size', () => {
-  window.matchMedia = (query) => query === 'somequery';
+  window.matchMedia = (query) => query === 'somequery' ? { matches: true } : { matches: false };
   expect(mediaQuery(['choice1'])).toEqual('choice1');
   expect(mediaQuery(['choice1', 'choice2'], ['somequery'])).toEqual('choice2');
   expect(mediaQuery(['choice1', 'choice2'], ['nonmatching'])).toEqual('choice1');
--- tests/styles.test.js
+++ tests/styles.test.js
@@ -2,5 +2,5 @@ import { percentOfViewWidth, percentOfViewHeight } from '../src/styles';
 
 test('percentOfViewWidth and percentOfViewHeight will return a string with viewport units attached', () => {
   expect(percentOfViewWidth(100)).toEqual('100vw');
-  expect(percentOfViewHeight(100).toEqal('100vh'));
+  expect(percentOfViewHeight(100)).toEqual('100vh');
 });
