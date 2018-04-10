66f01f6b77d53d884ae83792132b28c562fbe222
Daniel Fredriksen <dfredriksen@cyint.technology>
2018-04-10 08:56:54 -0700

    Added matches property to media query logic

--- src/mediaQuery.js
+++ src/mediaQuery.js
@@ -3,7 +3,7 @@ export const mediaQuery = (choices, dimensions) => {
   let selectedChoice = choices[0];
   if (!isEmpty(dimensions)) {
     dimensions.forEach((query, index) => {
-      if (window.matchMedia(query)) {
+      if (window.matchMedia(query).matches) {
         selectedChoice = choices[index + 1];
       }
     });
